f003f9c10bce110329ab4745bca2d00b75b2750b
PhatCT24 <tienphat0812004@gmail.com>
2024-11-27 23:42:01 +0700

    update confirm form update

--- src_code/src/gui/confirmForm/UpdateBookConfirm.java
+++ src_code/src/gui/confirmForm/UpdateBookConfirm.java
@@ -8,12 +8,12 @@ package gui.confirmForm;
  *
  * @author DELL
  */
-public class removeUserConfirm extends javax.swing.JFrame {
+public class UpdateBookConfirm extends javax.swing.JFrame {
 
     /**
      * Creates new form removeBookConfirm
      */
-    public removeUserConfirm() {
+    public UpdateBookConfirm() {
         initComponents();
     }
 
@@ -33,7 +33,7 @@ public class removeUserConfirm extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel1.setText("Are you sure you want to remove this User?");
+        jLabel1.setText("Are you sure you want to remove this Book?");
 
         nobutton.setText("No");
         nobutton.addActionListener(new java.awt.event.ActionListener() {
@@ -97,20 +97,21 @@ public class removeUserConfirm extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(removeBookConfirm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UpdateBookConfirm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(removeBookConfirm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UpdateBookConfirm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(removeBookConfirm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UpdateBookConfirm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(removeBookConfirm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UpdateBookConfirm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new removeUserConfirm().setVisible(true);
+                new UpdateBookConfirm().setVisible(true);
             }
         });
     }
